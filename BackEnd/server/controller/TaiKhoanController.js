8fa36989f08a0e1fb9085a172b6c004b7dedd247
TuanHung <hungbuituan1@gmail.com>
2022-11-20 20:34:15 +0700

    merge button

--- BackEnd/server/controller/TaiKhoanController.js
+++ BackEnd/server/controller/TaiKhoanController.js
@@ -7,12 +7,18 @@ class TaiKhoanControllers {
     index(req, res) {
         res.send('TaiKhoan')
     }
-    capTaiKhoan = async (req, res) => {
-        //có tài khoản rồi thì không cấp cho nữa
+    capTaiKhoanOrResetMatKhau = async (req, res) => {
+        //có tài khoản rồi thì sẽ chuyển qua resetmk
         let rs = await DB.query(`SELECT * FROM TAIKHOAN WHERE TENDANGNHAP = '${req.body.TENDANGNHAP}'`)
         if (rs.length > 0) {
-            // res.send(json(false, "Nhân viên đã có tài khoản trước đó"))
-            return json(false, "Nhân viên đã có tài khoản trước đó");
+            //random và mã hóa mật khẩu
+            let MATKHAU = Date.now().toString(36);
+            let salt = await bcrypt.genSalt(10);
+            let ecryptedMATKHAU = await bcrypt.hash(MATKHAU, salt);
+            rs = await DB.query(`UPDATE TAIKHOAN SET MATKHAU = '${ecryptedMATKHAU}' WHERE TENDANGNHAP ='${req.body.TENDANGNHAP}'`)
+            console.log("Cấp lại mật khẩu thành công, mật khẩu mới: " + MATKHAU);
+            // res.send(json(true,"Cấp lại mật khẩu thành công, mật khẩu mới: " + MATKHAU))
+            return;
         }
         //random và mã hóa mật khẩu
         let MATKHAU = Date.now().toString(36);
@@ -20,26 +26,10 @@ class TaiKhoanControllers {
         let ecryptedMATKHAU = await bcrypt.hash(MATKHAU, salt);
         rs = await DB.query(`insert into TAIKHOAN(TENDANGNHAP,MATKHAU) values ('${req.body.TENDANGNHAP}','${ecryptedMATKHAU}')`)
 
-        console.log("Cấp tài khoản:" + MATKHAU);
-        // res.send(json(true, MATKHAU))
+        console.log("Cấp tài khoản thành công, mật khẩu:" + MATKHAU);
+        // res.send(json(true, "Cấp tài khoản thành công, mật khẩu:" + MATKHAU))
     }
-    capLaiMatKhau = async (req, res) => {
 
-        let rs = await DB.query(`SELECT * FROM TAIKHOAN WHERE TENDANGNHAP = '${req.body.TENDANGNHAP}'`)
-        if (rs.length == 0) {
-            // res.send(json(false, "Nhân viên chưa có tài khoản"))
-            return json(false, "Nhân viên chưa có tài khoản");
-        }
-
-        //random và mã hóa mật khẩu
-        let MATKHAU = Date.now().toString(36);
-        let salt = await bcrypt.genSalt(10);
-        let ecryptedMATKHAU = await bcrypt.hash(MATKHAU, salt);
-        rs = await DB.query(`UPDATE TAIKHOAN SET MATKHAU = '${ecryptedMATKHAU}' WHERE TENDANGNHAP ='${req.body.TENDANGNHAP}'`)
-
-        console.log("Cấp lại mật khẩu:" + MATKHAU);
-        // res.send(json(true,MATKHAU))
-    }
     doiMatKhau = async (req, res) => {
         let salt = await bcrypt.genSalt(10);
         req.body.MATKHAU = await bcrypt.hash(req.body.MATKHAU, salt);
@@ -47,33 +37,22 @@ class TaiKhoanControllers {
         console.log("Đổi mật khẩu tài khoản:" + req.body.TENDANGNHAP);
         // res.send(json())
     }
-    khoaTaiKhoan = async (req, res) => {
+    khoaOrMoKhoaTaiKhoan = async (req, res) => {
         let rs = await DB.query(`SELECT * FROM TAIKHOAN WHERE TENDANGNHAP = '${req.body.TENDANGNHAP}'`)
         if (rs.length == 0) {
+            console.log(json(false, "Nhân viên không có tài khoản"))
             // res.send(json(false, "Nhân viên không có tài khoản"))
-            return json(false, "Nhân viên không có tài khoản");
+            return;
         }
         if (rs[0].KHOA) {
-            // res.send(json(false, "Tài khoản đã bị khóa trước đó"));
-            return json(false, "Tài khoản đã bị khóa trước đó");
+            rs = await DB.query(`UPDATE TAIKHOAN SET KHOA ='false' WHERE TENDANGNHAP='${req.body.TENDANGNHAP}'`)
+            console.log("Mở Khóa tài khoản:" + req.body.TENDANGNHAP);
+            // res.send(json(true,"Đã mở khóa tài khoản: " + req.body.TENDANGNHAP))
+            return;
         }
         rs = await DB.query(`UPDATE TAIKHOAN SET KHOA ='true' WHERE TENDANGNHAP='${req.body.TENDANGNHAP}'`)
         console.log("Khóa tài khoản:" + req.body.TENDANGNHAP);
-        // res.send(json())
-    }
-    moKhoaTaiKhoan = async (req, res) => {
-        let rs = await DB.query(`SELECT * FROM TAIKHOAN WHERE TENDANGNHAP = '${req.body.TENDANGNHAP}'`)
-        if (rs.length == 0) {
-            // res.send(json(false, "Nhân viên không có tài khoản"))
-            return json(false, "Nhân viên không có tài khoản");
-        }
-        if (rs[0].KHOA == false) {
-            // res.send(json(false, "Tài khoản không bị khóa"));
-            return json(false, "Tài khoản không bị khóa");
-        }
-        rs = await DB.query(`UPDATE TAIKHOAN SET KHOA ='false' WHERE TENDANGNHAP='${req.body.TENDANGNHAP}'`)
-        console.log("Mở Khóa tài khoản:" + req.body.TENDANGNHAP);
-        // res.send(json())
+        // res.send(json(true,"Đã khóa tài khoản: " + req.body.TENDANGNHAP))
     }
     dangNhap = async (req, res) => {
         let rs = await DB.query(`SELECT * FROM TAIKHOAN WHERE TENDANGNHAP = '${req.body.TENDANGNHAP}'`)
