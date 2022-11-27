56f32b48528a42d3af71390d4750da75ad55bad1
TuanHung <hungbuituan1@gmail.com>
2022-11-27 16:13:18 +0700

    update FE

--- FrontEnd/demo/script.js
+++ FrontEnd/demo/script.js
@@ -20,11 +20,21 @@ import server from '../server/main.js'
 
 // });;
 
-server.DichVu.themDichVu({
-  // MAKH: 'KH01',
-  HOTEN: 'Nguyễn Văn B', GIOITINH: 'NAM', NAMSINH: '2001', SDT: '0973343549', HINHANH: './'
+// server.DichVu.xoaDichVu({
+//   MADV: 'DV00000010',
+//   // TENDV: 'DV01', SONGAYSUDUNG: '60', GIA: '1200000', HIENHANH: 0
+// }).then((result) => {
+//   console.log(result);
+// }).catch((err) => {
+// });;
+
+
+server.PhieuThuTien.thanhToan({
+  SOTIENTHU: 300000, MAPDK: 'PDK0000009', MANV: 'NV00000001'
 }).then((result) => {
   console.log(result);
 }).catch((err) => {
 });;
+
+
 
