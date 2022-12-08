e50fb60be6ab9ed836f437b6c5a13f51fc47d9c7
TuanHung <hungbuituan1@gmail.com>
2022-12-08 20:49:38 +0700

    Update UI khach hang

--- BackEnd/server/controller/ThongKeController.js
+++ BackEnd/server/controller/ThongKeController.js
@@ -9,13 +9,8 @@ class ThongKeControllers {
     xemThongKeKh = async (req, res) => {
         const { TUNGAY, DENNGAY } = req.body
         let params = [
-<<<<<<< HEAD
-            { name: 'TUNGAY', type: 'Date', value: TUNGAY},
-            { name: 'DENNGAY', type: 'Date', value: DENNGAY},
-=======
-            { name: 'TUNGAY', type: 'Date', value: ''},
-            { name: 'DENNGAY', type: 'Date', value: ''},
->>>>>>> 29b491d23787988fc26f1391b63e56e112026b92
+            { name: 'TUNGAY', type: 'Date', value: TUNGAY },
+            { name: 'DENNGAY', type: 'Date', value: DENNGAY },
         ]
         let rs = await DB.excute('SP_THONG_KE_KH', params)
         res.send(json(true, rs.recordset))
@@ -24,8 +19,8 @@ class ThongKeControllers {
     xemThongKeRaVao = async (req, res) => {
         const { TUNGAY, DENNGAY } = req.body
         let params = [
-            { name: 'TUNGAY', type: 'Date', value: TUNGAY},
-            { name: 'DENNGAY', type: 'Date', value: DENNGAY},
+            { name: 'TUNGAY', type: 'Date', value: TUNGAY },
+            { name: 'DENNGAY', type: 'Date', value: DENNGAY },
         ]
         let rs = await DB.excute('SP_LUOT_RA_VAO', params)
         res.send(json(true, rs.recordset))
--- FrontEnd/UI/KhachHang/KhachHang.js
+++ FrontEnd/UI/KhachHang/KhachHang.js
@@ -8,8 +8,9 @@ import server from "../../server/main.js";
 async function initUser() {
   let nhanVienBox = document.querySelectorAll('.admin_name span');
   let nhanVien = (await server.NhanVien.getList({ KEY: window.localStorage.getItem('username') })).data[0]
-  nhanVienBox[0].innerText = nhanVien.HOTEN;
-  nhanVienBox[1].innerText = nhanVien.MANV + ' - ' + nhanVien.CHUCVU;
+  console.log(nhanVien);
+  // nhanVienBox[0].innerText = nhanVien.HOTEN;
+  // nhanVienBox[1].innerText = nhanVien.MANV + ' - ' + nhanVien.CHUCVU;
 }
 
 
