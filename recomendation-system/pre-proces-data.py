985e35c2ac59c9a879718d83200812a74e36af53
TuanHung <hungbuituan1@gmail.com>
2022-12-16 13:47:40 +0700

    update data train

--- recomendation-system/pre-proces-data.py
+++ recomendation-system/pre-proces-data.py
@@ -16,14 +16,17 @@ features = ds.values[:, :5]
 labels = np.transpose(ds.values[:, 5])
 
 X_train, X_test, y_train, y_test = train_test_split(
-    features, labels, random_state=0)
+    features, labels, test_size=0.25, random_state=20)
 
 
-# for i in range(100):
-# model = KNeighborsClassifier(n_neighbors=i+1).fit(X_train, y_train)
+for i in range(30):
+    model = KNeighborsClassifier(n_neighbors=i+1).fit(X_train, y_train)
+    print("K = "+str(i+1)+", Score = " +
+          str(round(model.score(X_test, y_test), 2)))
+
+# model = KNeighborsClassifier(n_neighbors=12).fit(X_train, y_train)
 # print(model.score(X_test, y_test))
-model = KNeighborsClassifier(n_neighbors=12).fit(X_train, y_train)
-print(model.score(X_test, y_test))
+
 # save model
 dump(model, 'model.mol')
 
