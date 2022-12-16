855dab4921687794981c835516ea916266a595a0
vy nguyen <vy279.thpt@gmail.com>
2022-12-16 17:01:30 +0700

    Update export csv TPBS

--- FrontEnd/UI/ThucPhamBoSung/module.js
+++ FrontEnd/UI/ThucPhamBoSung/module.js
@@ -13,7 +13,6 @@ async function initNhanVien() {
 }
 initNhanVien();
 
-
 async function loadList(KEY) {
   let list = await server.ThucPhamBoSung.getList({ KEY });
   if (list.status) {
@@ -22,7 +21,7 @@ async function loadList(KEY) {
 
     for (let sp of list.data) {
       if (sp.HINHANH && sp.HINHANH.trim().length == 0) {
-        sp.HINHANH = 'defaultAvt.jpg'
+        sp.HINHANH = "defaultAvt.jpg";
       }
       out += `
         <tr>
@@ -97,8 +96,9 @@ async function initEvents() {
   var rows = document.getElementsByTagName("tbody")[0].rows;
 
   function ThemSp() {
-
-    const imageInput_AddPopup = document.querySelector(".popup-add .image-input");
+    const imageInput_AddPopup = document.querySelector(
+      ".popup-add .image-input"
+    );
     const image_AddPopup = document.querySelector(".popup-add img");
     const add_product_form = document.querySelector("#add-product-form");
 
@@ -106,9 +106,9 @@ async function initEvents() {
     imageInput_AddPopup.addEventListener("change", () => {
       var file = imageInput_AddPopup.files[0];
       image_AddPopup.src = URL.createObjectURL(file);
-    })
+    });
     add_product_form.addEventListener("submit", async (e) => {
-      console.log('submited add SP');
+      console.log("submited add SP");
       e.preventDefault();
       const sp = Object.fromEntries(new FormData(e.target));
       if (sp.TENSP.trim().length == 0) {
@@ -121,26 +121,22 @@ async function initEvents() {
       }
       //nếu k có ảnh thì set ảnh mặc định
 
-      if (sp.HINHANH.name != '')
-        sp.HINHANH = await uploadImg(sp.HINHANH)
-      else
-        sp.HINHANH = document.querySelector('.popup-add img').src;
+      if (sp.HINHANH.name != "") sp.HINHANH = await uploadImg(sp.HINHANH);
+      else sp.HINHANH = document.querySelector(".popup-add img").src;
 
       server.ThucPhamBoSung.themSanPham(sp).then((result) => {
         if (!result.status) {
           alert(result.data);
-          return
+          return;
         }
-        document.querySelector(".popup-add").classList.remove('show');
+        document.querySelector(".popup-add").classList.remove("show");
         alert("Thêm sản phẩm thành công");
         loadListSP("");
-      })
-
+      });
     });
-
   }
   // THEM SAN PHAM
-  ThemSp()
+  ThemSp();
 
   // CAP NHAT SAN PHAM
 
@@ -152,58 +148,60 @@ async function initEvents() {
       input[3].value = rows[x].getElementsByTagName("td")[0].innerText;
       input[4].value = rows[x].getElementsByTagName("td")[1].innerText;
       input[5].value = rows[x].getElementsByTagName("td")[2].innerText;
-      server.ThucPhamBoSung.getList({'KEY':rows[x].getElementsByTagName("td")[0].innerText}).then((rs)=>{
+      server.ThucPhamBoSung.getList({
+        KEY: rows[x].getElementsByTagName("td")[0].innerText,
+      }).then((rs) => {
         // console.log(rs.data[0].HINHANHrs.data[0].HINHANH)
         document.querySelector(".popup-update img").src = rs.data[0].HINHANH;
-      })
-      const imageInput_UpdatePopup = document.querySelector(".popup-update .image-input");
+      });
+      const imageInput_UpdatePopup = document.querySelector(
+        ".popup-update .image-input"
+      );
       const image_UpdatePopup = document.querySelector(".popup-update img");
       imageInput_UpdatePopup.addEventListener("change", () => {
         var file = imageInput_UpdatePopup.files[0];
         image_UpdatePopup.src = URL.createObjectURL(file);
-      })
-    })
-      // input[7].value = rows[x].getElementsByTagName("td")[3].innerText.split('.').join("").replace('VND','');
-    }
-
-      btn_update_form.addEventListener("submit", async (e) => {
-        console.log('submited update KH');
-        e.preventDefault();
-        e.stopImmediatePropagation()
-        const tp = Object.fromEntries(new FormData(e.target));
-        tp.MASP = document.querySelector("#MASP").value;
-        if (tp.TENSP.trim().length == 0) {
-          alert("Tên sản phẩm không được để trống");
-          return;
-        }
-        if (tp.MOTA.trim().length == 0) {
-          alert("Mô tả không được để trống");
-          return;
-        }
-        //nếu k có ảnh thì set ảnh mặc định
-        if (tp.HINHANH.name != '')
-        tp.HINHANH = await uploadImg(tp.HINHANH)
-        else
-        tp.HINHANH = document.querySelector('.popup-update img').src;
-        
-        let update = await server.ThucPhamBoSung.capNhatSanPham(tp)
-        if (!update.status) {
-              alert(rs.data);
-              popup_edit[0].classList.remove("show");
-            }
-            alert("Cập nhật thành công!");
-            popup_edit[0].classList.remove("show");
-            loadListSP('');
-        // server.ThucPhamBoSung.capNhatSanPham(tp).then((rs)=>{
-        //   if (!rs.status) {
-        //     alert(rs.data);
-        //     popup_edit[0].classList.remove("show");
-        //   }
-        //   alert("Cập nhật thành công!");
-        //   popup_edit[0].classList.remove("show");
-        //   loadListSP('');
-        // })
       });
+    });
+    // input[7].value = rows[x].getElementsByTagName("td")[3].innerText.split('.').join("").replace('VND','');
+  }
+
+  btn_update_form.addEventListener("submit", async (e) => {
+    console.log("submited update KH");
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    const tp = Object.fromEntries(new FormData(e.target));
+    tp.MASP = document.querySelector("#MASP").value;
+    if (tp.TENSP.trim().length == 0) {
+      alert("Tên sản phẩm không được để trống");
+      return;
+    }
+    if (tp.MOTA.trim().length == 0) {
+      alert("Mô tả không được để trống");
+      return;
+    }
+    //nếu k có ảnh thì set ảnh mặc định
+    if (tp.HINHANH.name != "") tp.HINHANH = await uploadImg(tp.HINHANH);
+    else tp.HINHANH = document.querySelector(".popup-update img").src;
+
+    let update = await server.ThucPhamBoSung.capNhatSanPham(tp);
+    if (!update.status) {
+      alert(rs.data);
+      popup_edit[0].classList.remove("show");
+    }
+    alert("Cập nhật thành công!");
+    popup_edit[0].classList.remove("show");
+    loadListSP("");
+    // server.ThucPhamBoSung.capNhatSanPham(tp).then((rs)=>{
+    //   if (!rs.status) {
+    //     alert(rs.data);
+    //     popup_edit[0].classList.remove("show");
+    //   }
+    //   alert("Cập nhật thành công!");
+    //   popup_edit[0].classList.remove("show");
+    //   loadListSP('');
+    // })
+  });
 
   // XOA SAN PHAM
   for (var i = 0; i < btn_delete.length; i++) {
@@ -226,19 +224,86 @@ async function initEvents() {
               alert(result.data);
             }
           })
-          .catch((err) => { });
+          .catch((err) => {});
       });
     });
   }
-}
 
+  // export
+  let export_btn = document.querySelector(".btn-export");
+  export_btn.addEventListener("click", () => {
+    let from = document.querySelector("#from").value;
+    let to = document.querySelector("#to").value;
+    console.log({from,to})
+    if(from.length==0||to.length==0){
+      alert("Bạn chưa nhập đầy đủ ngày!")
+      return
+    }
+    if(to<from){
+      alert("Ngày không hợp lệ!")
+      return
+    }
+    tableToCSV();
+  });
+
+  function tableToCSV() {
+    // Variable to store the final csv data
+    let from = document.querySelector("#from").value;
+    let to = document.querySelector("#to").value;
+    let csv = ["MAKH,TUOI,GIOITINH,BMI,TILEMO,TILECO,MASP,NGAY,GOIY_THANHCONG"];
+    server.ThucPhamBoSung.getListMachineLearning({
+      TUNGAY: from,
+      DENNGAY: to,
+    }).then((result) => {
+      for (let data of result.data) {
+        csv.push(
+          data.MAKH +
+            "," +
+            data.TUOI +
+            "," +
+            data.GIOITINH +
+            "," +
+            data.BMI +
+            "," +
+            data.TILEMO +
+            "," +
+            data.TILECO +
+            "," +
+            data.MASP +
+            "," +
+            data.NGAY +
+            "," +
+            data.GOIY_THANHCONG
+        );
+      }
+      csv = csv.join("\n");
+      var BOM = "\uFEFF";
+      var csvContent = BOM + csv;
+      downloadCSVFile(csvContent);
+    });
+  }
+  function downloadCSVFile(csv_data) {
+    let CSVFile = new Blob([csv_data], { type: "text/csv" });
+
+    var temp_link = document.createElement("a");
+
+    temp_link.download = "GfG.csv";
+    var url = window.URL.createObjectURL(CSVFile);
+    temp_link.href = url;
+
+    temp_link.style.display = "none";
+    document.body.appendChild(temp_link);
+    temp_link.click();
+    document.body.removeChild(temp_link);
+  }
+}
 
 async function loadListSP(KEY) {
   await loadList(KEY);
   initEvents();
 }
 
-loadListSP('');
+loadListSP("");
 
 const searchBar = document.querySelector(".search-box input");
 searchBar.addEventListener("keyup", () => {
